0e75ae98e0caa70ae4b29c41f1d0a1008ff2aa2d
Esteban Portillo <esteban.07.elias@gmail.com>
2022-10-03 09:35:25 -0600

    clase 19: escuchando eventos con JavaScript + Solucion Reto

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,28 @@
-function seleccionarMascotaJugador() {
-    alert('SELECCIONASTE TU MASCOTA');
+function iniciarJuego() {
+    let btnMascotaJugador = document.getElementById('btnMascota');
+    btnMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 }
 
-let btnMascotaJugador = document.getElementById('btnMascota');
-btnMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
+// Mi solución
+function seleccionarMascotaJugador() {
+    // #1: Utilizando arrays de mascotas y retornando la seleccionada
+    let mascotas = document.querySelectorAll('input');
+    for (let mascota of mascotas) {
+        if (mascota.checked) {
+            return console.log(primerLetraMayuscula(mascota.id));
+        }
+    }
+
+    // #2: Evaluando si existe algún input checked en el DOM
+    // para imprimir el valor del elemento seleccionado
+    let mascotaSeleccionada = document.querySelector('input:checked');
+    if (mascotaSeleccionada) {
+        console.log(primerLetraMayuscula(mascotaSeleccionada.id));
+    }
+}
+
+function primerLetraMayuscula(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+window.addEventListener('load', iniciarJuego);
