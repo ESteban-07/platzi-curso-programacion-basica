8f2412a79a478ad89ce6ad8dc9132b8af0f399d5
Esteban Portillo <esteban.07.elias@gmail.com>
2022-10-03 12:07:33 -0600

    clase 22: enemigos aleatorios

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -9,7 +9,6 @@ function seleccionarMascotaJugador() {
     let inputCapipepo = document.getElementById('capipepo');
     let inputRatigueya = document.getElementById('ratigueya');
     let spanMascotaJugador = document.getElementById('mascotaJugador');
-    let mascotaEnemigo = document.getElementById('mascotaEnemigo');
 
     if (inputHipodoge.checked) {
         alert('Seleccionaste a Hipodoge');
@@ -23,6 +22,33 @@ function seleccionarMascotaJugador() {
     } else {
         alert('Selecciona una mascota');
     }
+
+    // Explicación: Sí la etiqueta <span> que contiene el nombre de la mascota del
+    // jugador NO se encuentra vacía, entonces el enemigo puede elegir su mascota.
+    // En caso contrario, no se ejecutará la función seleccionarMascotaEnemigo().
+    if (!(spanMascotaJugador.innerText == '')) {
+        seleccionarMascotaEnemigo();
+    }
+}
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1, 3);
+    let spanMascotaEnemigo = document.getElementById('mascotaEnemigo');
+
+    if (ataqueAleatorio == 1) {
+        // Hipodoge
+        spanMascotaEnemigo.innerHTML = 'Hipodoge';
+    } else if (ataqueAleatorio == 2) {
+        // Capipepo
+        spanMascotaEnemigo.innerHTML = 'Capipepo';
+    } else {
+        // Ratigueya
+        spanMascotaEnemigo.innerHTML = 'Ratigueya';
+    }
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 window.addEventListener('load', iniciarJuego);
