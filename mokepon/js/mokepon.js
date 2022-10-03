860b1f47b87d7d359d8672261471540c342188fc
Esteban Portillo <esteban.07.elias@gmail.com>
2022-10-03 11:27:24 -0600

    clase 21: manipulación del DOM

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -8,12 +8,18 @@ function seleccionarMascotaJugador() {
     let inputHipodoge = document.getElementById('hipodoge');
     let inputCapipepo = document.getElementById('capipepo');
     let inputRatigueya = document.getElementById('ratigueya');
+    let spanMascotaJugador = document.getElementById('mascotaJugador');
+    let mascotaEnemigo = document.getElementById('mascotaEnemigo');
+
     if (inputHipodoge.checked) {
         alert('Seleccionaste a Hipodoge');
+        spanMascotaJugador.innerText = 'Hipodoge';
     } else if (inputCapipepo.checked) {
         alert('Seleccionaste a Capipepo');
+        spanMascotaJugador.innerText = 'Capipepo';
     } else if (inputRatigueya.checked) {
         alert('Seleccionaste a Ratigueya');
+        spanMascotaJugador.innerText = 'Ratigueya';
     } else {
         alert('Selecciona una mascota');
     }
